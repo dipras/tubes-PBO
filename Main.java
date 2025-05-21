82dcbd6329f2e6d9c59ff3899cb2faad45e80cd1
Aldy Prastyo <dipras75@gmail.com>
2025-05-21 19:30:42 +0700

    Feat: add second category to main driver

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ import pkg.Category;
 public class Main {
     public static void main(String[] kontoru) {
         Category category1 = new Category(1, "Fast Food");
+        Category category2 = new Category(2, "Gorengan");
 
         System.out.println(category1.toString());
     }
