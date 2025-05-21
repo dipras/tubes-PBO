cbe841e2a2dd2442256302c6b3d393d1c5f650ed
Aldy Prastyo <dipras75@gmail.com>
2025-05-21 19:21:34 +0700

    Feat: add main driver class

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+import pkg.Category;
+
+
+public class Main {
+    public static void main(String[] kontoru) {
+        Category category1 = new Category(1, "Fast Food");
+
+        System.out.println(category1.toString());
+    }
+}
