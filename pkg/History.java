4b01608ac606e4a754f2b83a8d7486c19547779d
SiRadit <dharmawanraditya123@gmail.com>
2025-05-22 14:01:02 +0700

    Feat: add History class

---
+++ pkg/History.java
@@ -0,0 +1,57 @@
+package pkg;
+
+import java.util.Date;
+
+public class History {
+
+    private int id = 0;
+    private int crated_by = 0;
+    private String method_payment = "";
+    private int cashcharge = 0;
+    private int cashback = 0;
+    private int total = 0;
+    private Date created_at = null;
+    private Date update_at = null;
+
+    public History(int id, int crated_by, String method_payment, int cashcharge, int cashback, int total) {
+        this.id = id;
+        this.crated_by = crated_by;
+        this.method_payment = method_payment;
+        this.cashcharge = cashcharge;
+        this.cashback = cashback;
+        this.total = total;
+        this.created_at = new Date();
+        this.update_at = new Date();
+    }
+
+    public int getCrated_by() {
+        return crated_by;
+    }
+
+    public String getMethod_payment() {
+        return method_payment;
+    }
+
+    public int getCashcharge() {
+        return cashcharge;
+    }
+
+    public int getCashback() {
+        return cashback;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "id = " + id + "; Crate by =" + crated_by + "; Method Payment =" + method_payment + "; Cash Charge" + cashcharge + "; Cash Back = "
+                + cashback + "Total = " + total + "; Created At = " + created_at + "; Updated At = " + update_at;
+    }
+
+}
