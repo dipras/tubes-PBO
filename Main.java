7ec90ba92689eea3fbaea515fe1540e813c0c85c
SiRadit <dharmawanraditya123@gmail.com>
2025-06-04 12:15:53 +0700

    Feat for user main

--- Main.java
+++ Main.java
@@ -1,12 +1,66 @@
 
-import pkg.Category;
-
+import java.util.Scanner;
+import pkg.User;
 
 public class Main {
-    public static void main(String[] kontoru) {
-        Category category1 = new Category(1, "Fast Food");
-        Category category2 = new Category(2, "Gorengan");
 
-        System.out.println(category1.toString());
+    static User[] users = {new User(1, "Raddit", "radit@gmail.com", "wkowko")};
+    static User currentUser;
+    static Scanner inp = new Scanner(System.in);
+
+    public static void main(String[] kontoru) {
+        loginPage();
+        System.out.println("SELEMAT DATANG " + currentUser.getName());
+        mainMenu();
+    }
+
+    public static void loginPage() {
+        System.out.println("======================================");
+        System.out.println("LOGIN PAGE");
+        System.out.println("======================================");
+
+        System.out.print("email: ");
+        String email = inp.nextLine();
+        System.out.print("password: ");
+        String pw = inp.nextLine();
+
+        for (User user : users) {
+            if (user.getEmail().equals(email)) {
+                if (user.isPasswordValid(pw)) {
+                    currentUser = user;
+                    System.out.println("BERHASIL LOGIN\n\n");
+                }
+                break;
+            }
+        }
+
+        if (currentUser == null) {
+            System.out.println("Email atau password salah!\n\n");
+
+            loginPage();
+        }
+    }
+
+    public static void mainMenu() {
+        System.out.println("==============================================");
+        System.out.println("1. Stock\n2. Checkout\n3. Profile\n9. Logout");
+        System.out.println("==============================================");
+
+        int choice = 0;
+        System.out.print("## Masukan pilihan: ");
+        choice = inp.nextInt();
+        inp.nextLine();
+        switch (choice) {
+            case 1 ->
+                System.out.println("Hello world");
+            case 9 ->
+                System.out.println("Selamat tinggal!");
+            default ->
+                System.out.println("Masukan menu yang benar!");
+        }
+
+        if (choice != 9) {
+            mainMenu();
+        }
     }
 }
---
+++ pkg/User.java
@@ -0,0 +1,36 @@
+package pkg;
+
+public class User {
+
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+
+    public User(int id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isPasswordValid(String pw) {
+        return pw.equals(password);
+    }
+}
