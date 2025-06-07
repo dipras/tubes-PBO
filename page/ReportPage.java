947046390568ba6110b485a06e49e69d1c77fb81
gector727 <gectorashera@gmail.com>
2025-06-07 13:43:09 +0700

    Create ReportPage.java

---
+++ page/ReportPage.java
@@ -0,0 +1,9 @@
+package page;
+
+public class ReportPage {
+    public static void view() {
+        System.out.println("Report Page");
+        System.out.println("This page is under construction.");
+        System.out.println("Please check back later for updates.");
+    }
+}
