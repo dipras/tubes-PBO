8a1ec2b31b746c50956665a03a2ad869a7a723b9
gector727 <gectorashera@gmail.com>
2025-06-07 11:18:25 +0700

    edit: dataType to int from Date | gross_profit

--- pkg/Report.java
+++ pkg/Report.java
@@ -4,14 +4,14 @@ import java.util.Date;
 public class Report {
     private int id = 0;
     private String name;
-    private Date gross_profit = null;
+    private int gross_profit;
     private Date created_at = null;
     private Date updated_at = null;
     
-    public Report(int id, String name) {
+    public Report(int id, String name, int gross_profit) {
         this.id = id;
         this.name = name;
-        this.gross_profit = new Date();
+        this.gross_profit = gross_profit;
         this.created_at = new Date();
         this.updated_at = new Date();
     }
@@ -25,10 +25,10 @@ public class Report {
         this.updated_at = new Date();
         this.name = name;
     }
-    public Date getGrossProfit() {
+    public int getGrossProfit() {
         return gross_profit;
     }
-    public void setGrossProfit(Date gross_profit) {
+    public void setGrossProfit(int gross_profit) {
         this.updated_at = new Date();
         this.gross_profit = gross_profit;
     }
