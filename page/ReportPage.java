a960448baf73e359856c84f51faae8fb110edc9e
gector727 <gectorashera@gmail.com>
2025-06-07 14:12:24 +0700

    feat: ReportPage class

--- page/ReportPage.java
+++ page/ReportPage.java
@@ -1,9 +1,11 @@
 package page;
 
+
 public class ReportPage {
     public static void view() {
         System.out.println("Report Page");
         System.out.println("This page is under construction.");
         System.out.println("Please check back later for updates.");
+
     }
 }
